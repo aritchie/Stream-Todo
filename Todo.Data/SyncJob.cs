469abd454111ddb56b3a6ede22a5456bd64160e0
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-01 12:43:34 -0400

    initial implementation of sync job

--- Todo.Data/SyncJob.cs
+++ Todo.Data/SyncJob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Shiny.Jobs;
@@ -8,11 +9,11 @@ namespace Todo.Data
 {
     public class SyncJob : IJob
     {
-        readonly IDataService data;
+        readonly TodoSqliteConnection data;
         readonly IApiClient apiClient;
 
 
-        public SyncJob(IApiClient apiClient, IDataService data)
+        public SyncJob(IApiClient apiClient, TodoSqliteConnection data)
         {
             this.apiClient = apiClient;
             this.data = data;
@@ -21,7 +22,34 @@ namespace Todo.Data
 
         public async Task<bool> Run(JobInfo jobInfo, CancellationToken cancelToken)
         {
-            return false;
+            var pushed = await this.Push(jobInfo, cancelToken);
+            var pulled = await this.Pull(jobInfo, cancelToken);
+            return pushed || pulled;
+        }
+
+
+        async Task<bool> Push(JobInfo jobInfo, CancellationToken cancelToken)
+        {
+            var todos = await this.data
+                .Todos
+                .Where(x => x.DateUpdatedUtc > jobInfo.LastRunUtc)
+                .ToListAsync();
+
+            var en = todos.GetEnumerator();
+            while (!cancelToken.IsCancellationRequested && en.MoveNext())
+                await this.apiClient.Save(en.Current);
+
+            return todos.Any();
+        }
+
+
+        async Task<bool> Pull(JobInfo jobInfo, CancellationToken cancelToken)
+        {
+            var todos = await this.apiClient.Get(true, jobInfo.LastRunUtc);
+            foreach (var todo in todos)
+                await this.data.InsertOrReplaceAsync(todo);
+
+            return todos.Any();
         }
     }
 }
